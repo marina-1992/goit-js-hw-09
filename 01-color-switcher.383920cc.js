7b769ca1a91b6f1ff9d76c7579beb6adefd2190f
marina-1992 <125686496+marina-1992@users.noreply.github.com>
2023-07-28 11:29:09 +0000

    Deploying to gh-pages from @ marina-1992/goit-js-hw-09@e0ed1b80586a69989e411274b3cb3ff443b728d8 🚀

---
+++ 01-color-switcher.383920cc.js
@@ -0,0 +1,2 @@
+let t=null;const e={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")};e.stopBtn.disabled=!0,e.startBtn.addEventListener("click",(function(){e.startBtn.disabled=!0,e.stopBtn.disabled=!1,t=setInterval((()=>{e.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.stopBtn.addEventListener("click",(function(){e.startBtn.disabled=!1,e.stopBtn.disabled=!0,clearInterval(t)}));
+//# sourceMappingURL=01-color-switcher.383920cc.js.map
